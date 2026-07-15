9003e0c28b82d92b389f46b935d8316e50b98572
vishal <saxenaji789@gmail.com>
2026-07-15 13:17:50 +0530

    integrate frontend with bacend using axios

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,12 @@
 import express from "express";
+import cors from "cors";
 import urlRoutes from "./routes/urlRoutes.js";
+
 const app = express();
 
+
 //middleware
+app.use(cors());
 app.use(express.json());
 
 //Routes
